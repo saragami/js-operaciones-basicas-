3b0416f38128ea6f869e67d3a392cdc7d390f57a
sara gam <98971106+saragami@users.noreply.github.com>
2022-02-17 14:48:20 -0600

    Add files via upload
    basic js

---
+++ js_1/main.js
@@ -0,0 +1,26 @@
+
+//declara la variable de una suma
+
+var=1+2 
+//suma de numero x y y
+let x = prompt("ingresar un numero");
+
+let y = prompt("ingresa un segundo numero");
+
+let resultado =parseFloat(x)+parsetFloat(y);
+
+console.log("la suma es")+(resultado);
+let edad= prompt("ingresa tu edad");
+let nombre= prompt("ingresa tu nombre")
+let direccion= prompt("ingresa tu direccion")
+let precio= prompt("ingresa el precio de un bolillo")
+
+//Calcular el área de un triángulo. Los valores deben de ser capturados con promt y mostrados con alertas
+let lado= prompt("ingresa la medida de un lado del triangulo")
+let altura=prompt("ingresa la altura de un triangulo")
+let resultado1=(lado*altura);
+let resultado2=(resultado1/2);
+alert (resultado2);
+
+
+
